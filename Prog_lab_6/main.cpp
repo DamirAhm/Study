6b6a48d9d5ac1e31fb811be45dedd6b4aa7c87d0
damirahm <damir221003@gmail.com>
2022-02-25 16:33:29 +0300

    Updated 6th lab

--- Prog_lab_6/main.cpp
+++ Prog_lab_6/main.cpp
@@ -6,30 +6,47 @@
 
 using namespace std;
 
-string replace_space_groups(string str, int len, ofstream &out) {
-  bool last_was_space = false;
+const int n = 300;
 
-  string new_string = "";
+struct strm {
+  char mark;
+  char s[n + 1];
+};
+
+strm replace_space_groups(strm &text, int to_read, ofstream &out) {
+  bool last_was_space = false;
   bool group_logged = false;
 
-  for (int i = 0; i < len; i++) {
-    if (str[i] == ' ') {
-      if (!last_was_space) {
-        new_string += str[i];
-        last_was_space = true;
-      } else if (!group_logged) {
-        cout << "Найдена группа пробелов на позиции: " << i + 1 << endl;
-        out << "Найдена группа пробелов на позиции: " << i + 1 << endl;
-        group_logged = true;
-      }
-    } else {
-      new_string += str[i];
-      last_was_space = false;
-      group_logged = false;
-    }
-  }
+  int i = 0;
 
-  return new_string;
+  while (text.s[i] == ' ') i++;
+
+  do {
+    if (text.s[i] == ' ') {
+      int j = 1;
+      while (text.s[i + j] == ' ') j++; 
+      
+      if (text.s[i + j] == text.mark) return text;
+      j--;
+      int p = 1;
+      while (text.s[i + j + p - 1] != text.mark) {
+        text.s[i + p] = text.s[i + j + p];
+        p++;
+      }
+    }
+    i++;
+  } while (text.s[i] != text.mark && (to_read == -1 || i < max(to_read, 0)));
+    
+  return text;
+}
+
+void print_text(strm text, ofstream &out) {
+  int i = -1;
+  do {
+    i++;
+    cout << text.s[i];
+    out << text.s[i];
+  } while (text.s[i] != text.mark);
 }
 
 int main() {
@@ -52,44 +69,67 @@ int main() {
       << "Задание: 16.б\n";
 
   int flag = 0;
+  int to_read = -1;
+  char mark = '*';
 
-  cout << "Введите 1 если хотите работать с 1 версией программы и 2 если хотите работать со 2 версией" << endl;
+  cout << "Введите 1 если хотите работать по длинне и 2 если хотите работать с маркерами" << endl;
   cin >> flag;
+
+  if (flag == 1) {
+    cout << "Введите количество считываемых символов: ";
+    cin >> to_read;
+  } else {
+    cout << "Введите маркер: ";
+    cin >> mark;
+  }
   
-  string str = "";
+  strm text;
+  text.mark = mark;
+  
   int len = 0;
   int pos = 0;
   bool found_start = false;
   
-  while (!in.eof()) {
+  while (!in.eof() && len <= to_read) {
     pos++;
     char c;
     in >> resetiosflags(ios::skipws) >> c;
     if (flag == 2) {
       if (found_start) {
-        if (c == '#') {
+        if (c == text.mark) {
           cout << "Найден маркер на позиции - " << pos << ", заканчивается запись строки" << endl;
+          text.s[len] = c;
+          len++;
           break;
         };
-        str += c;
+        text.s[len] = c;
         len++;
-      } else if (c == '#') {
+      } else if (c == text.mark) {
         found_start = true;
         cout << "Найден маркер на позиции - " << pos << ", начинается запись строки" << endl;
       }
     } else {
-      str += c;
+      text.s[len] = c;
       len++;
     }
   }
 
-  cout << "Считанная строка: \n" << str << endl;
-  out << "Считанная строка: \n" << str << endl;
+  if (flag == 1 && to_read > len) {
+    text.s[len] = text.mark;
+  }
+  
+  cout << "Считанная строка: \n";
+  out << "Считанная строка: \n";
+  print_text(text, out);
+  cout << endl;
+  out << endl;
 
-  string changed_str = replace_space_groups(str, len, out);
+  replace_space_groups(text, to_read, out);
 
-  cout << "Строка с замененными группами пробелов: \n" << changed_str;
-  out << "Строка с замененными группами пробелов: \n" << changed_str;
+  cout << "Строка с замененными группами пробелов: \n";
+  out << "Строка с замененными группами пробелов: \n";
+  print_text(text, out);
 
   return 0;
 }
+
